0e6c2bbac037f22059c7046e7ec81b286b1c635d
OlgaSuslova <alekssportmen@gmail.com>
2022-11-29 15:52:18 +0300

    added file  program

---
+++ program/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача: написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте алгоритма.  
+
+Console.Clear();
+
+string[] array = {"hello", "2", "world", ":-)"};
+int sortnumber = 3; 
+Console.Write($"[{String.Join(", ", array)}]");
+
+
+Console.Write(" -> ");
+
+string[] newarray = SortedArray(array, sortnumber);
+Console.Write($"[{String.Join(", ", newarray)}]");
+
+
+////////////////////////////
+
+static string[] SortedArray (string[] array, int sortnumber)
+{ 
+    string[] newarray = new string [array.Length];
+    int count = 0; 
+    for (int i = 0; i < array.Length; i++)
+        {      
+            if(array[i].Length <= sortnumber)
+            {
+                newarray[count] = array[i];
+                count++;        
+            }
+        }
+    Array.Resize(ref newarray, count);
+    return newarray; 
+}
